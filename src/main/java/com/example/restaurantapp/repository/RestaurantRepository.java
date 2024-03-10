8ecedd2dfdbf287cde3ac3ced504a0aab2d6752d
mateuszskrzynsky <mateusz.skrzynsky@outlook.com>
2024-03-10 19:54:11 +0100

    3. Repositories added.

---
+++ src/main/java/com/example/restaurantapp/repository/RestaurantRepository.java
@@ -0,0 +1,10 @@
+package com.example.restaurantapp.repository;
+
+import com.example.restaurantapp.model.Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+
+}
---
+++ src/main/java/com/example/restaurantapp/repository/TableRepository.java
@@ -0,0 +1,9 @@
+package com.example.restaurantapp.repository;
+
+import com.example.restaurantapp.model.Table;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TableRepository extends JpaRepository<Table, Long> {
+}
